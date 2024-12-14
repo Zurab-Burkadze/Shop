6276f314ad40021c11cc8f6a8fb80f4aaf4c1dfc
zuka <zurab.burkadze2009@gmail.com>
2024-12-14 17:30:24 +0400

    fifth commit

--- products/views.py
+++ products/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products' : products})
+    product_name = request.GET.get('product_name')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    category = request.GET.get('category')
+    in_stock = request.GET.get('in_stock')
+
+    filters = dict()
+
+    if product_name:
+        filters['name__icontains'] = product_name
+
+    if min_price:
+        filters['price__gt'] = float(min_price)
+
+    if min_price:
+        filters['price__lt'] = float(max_price)
+
+    if in_stock == 'on':
+        filters['stock_qty__gt'] = 0
+
+    if category:
+        filters['category'] = category
+
+    products = Product.objects.filter(**filters)
+
+    categories = Category.objects.all()
+
+    return render(request, 'home.html', {'products' : products, 'categories' : categories})
 
 def product_detail(request, product_id):
-
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'product_detail.html', context={'product' : product})
 
