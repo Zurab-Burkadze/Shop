e7bd98f828a1caaa5911c73778a082222aa5a706
zuka <zurab.burkadze2009@gmail.com>
2024-12-16 20:04:41 +0400

    sixth commit

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Category
+from .forms import ProductForm
+from django.contrib import messages
+
 
 def home(request):
     products = Product.objects.all()
@@ -28,6 +31,11 @@ def home(request):
 
     products = Product.objects.filter(**filters)
 
+    sort_by = request.GET.get('sort')
+
+    if sort_by:
+        products = products.order_by(sort_by)
+
     categories = Category.objects.all()
 
     return render(request, 'home.html', {'products' : products, 'categories' : categories})
@@ -35,4 +43,16 @@ def home(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'product_detail.html', context={'product' : product})
+
+def create_product(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Product created successfully')
+
+            return redirect('home')
+
+    return render(request, 'create_product.html', context={'form' : form})
 
